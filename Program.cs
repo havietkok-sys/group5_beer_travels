f5b971cbcf049fbb7af6d4e60a9f1eef7047b0c4
Meti00a <muhamedhajra00@gmail.com>
2025-12-12 15:19:14 +0100

    fixed an issue

--- Program.cs
+++ Program.cs
@@ -48,6 +48,10 @@ app.MapGet("/beers", Beers.GetAll);
 app.MapGet("/beers/{id}", Beers.Get);
 app.MapDelete("/beers/{id}", Beers.Delete); //admin
 
+// crud Flavor
+app.MapPost("/flavors/create", beer_flavors.CreateFlavor); 
+app.MapDelete("/flavors/{id}", beer_flavors.DeleteFlavor); 
+app.MapGet("/flavors", beer_flavors.GetAllFlavors);
 
 // Skapa hotel 
 app.MapPost("/hotels/create", Hotels.CreateHotel); //Admin
